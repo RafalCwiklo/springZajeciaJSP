012ca1579fb12f789d4874dc4780608c1df9a081
RafalCwiklo <rafalcwiklo@gmail.com>
2023-02-08 20:24:55 +0100

    spring jsp project

---
+++ src/main/java/com/example/studentappspring4/StudentAppSpring4Application.java
@@ -0,0 +1,13 @@
+package com.example.studentappspring4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentAppSpring4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentAppSpring4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studentappspring4/StudentAppSpring4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentappspring4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentAppSpring4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
